b45e81c631184d3e69e7fd63e8be5dfa907130fd
harry1498 <hardeephardy8@gmail.com>
2021-10-26 09:16:46 +0530

    second commit

--- app/src/main/java/com/example/dummy/MainActivity2.java
+++ app/src/main/java/com/example/dummy/MainActivity2.java
@@ -17,35 +17,11 @@ import com.example.dummy.databinding.ActivityMain2Binding;
 
 public class MainActivity2 extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMain2Binding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        binding = ActivityMain2Binding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        setContentView(R.layout.activity_main2);
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
 }
